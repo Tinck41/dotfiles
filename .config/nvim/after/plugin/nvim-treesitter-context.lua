2b96504b39c36cd58da0ad021a95bc384ba3b3ed
Tinck41 <glushkov2000@bk.ru>
2025-04-16 10:17:47 +0500

    fix input lag

--- .config/nvim/after/plugin/nvim-treesitter-context.lua
+++ .config/nvim/after/plugin/nvim-treesitter-context.lua
@@ -1,1 +1,1 @@
-vim.keymap.set({"i", "n"}, "<leader>c", function() require("treesitter-context").toggle() end)
+vim.keymap.set("n", "<leader>c", function() require("treesitter-context").toggle() end)
